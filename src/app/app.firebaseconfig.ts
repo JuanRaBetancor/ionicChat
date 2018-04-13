ac21ca63664b5b8a3d3d753e583fe5fd56ebe3cb
JuanRaBetancor <jr-ulpgc@hotmail.com>
2018-04-13 17:15:33 +0000

    Initial commit - LogIn

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+  apiKey: "AIzaSyA-8sU55of4nb8yOFVsfzq4k-Iwi7G0EYc",
+  authDomain: "ionicchat-d641c.firebaseapp.com",
+  databaseURL: "https://ionicchat-d641c.firebaseio.com",
+  projectId: "ionicchat-d641c",
+  storageBucket: "ionicchat-d641c.appspot.com",
+  messagingSenderId: "211224633399"
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { config } from './app.firebaseconfig';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
+
+import { MyApp } from './app.component';
+import { AuthProvider } from '../providers/auth/auth';
+
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp, {tabsPlacement: 'top'}),
+    AngularFireModule.initializeApp(config)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    AngularFireAuth,
+    AuthProvider
+  ]
+})
+export class AppModule {}
