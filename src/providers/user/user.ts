2138c380c61ef5756e5a8c5621813f17b062d0aa
JuanRaBetancor <jr-ulpgc@hotmail.com>
2018-04-14 12:14:23 +0000

    All user pages done!

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -107,4 +107,23 @@ export class UserProvider {
     return promise;
   }
 
+
+  getallusers() {
+    var promise = new Promise((resolve, reject) => {
+      this.firedata.orderByChild('uid').once('value', (snapshot) => {
+        let userdata = snapshot.val();
+        let temparr = [];
+        for (var key in userdata) {
+          temparr.push(userdata[key]);
+        }
+        resolve(temparr);
+      }).catch((err) => {
+        reject(err);
+      })
+    })
+    return promise;
+  }
+
+
+
 }
