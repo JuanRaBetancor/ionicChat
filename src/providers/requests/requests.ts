d7bafa279d2e423781efec26fbe2660b87bcf9ac
JuanRaBetancor <jr-ulpgc@hotmail.com>
2018-04-14 18:04:03 +0000

    Adding friends complete

--- src/providers/requests/requests.ts
+++ src/providers/requests/requests.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Events } from 'ionic-angular';
 import { connreq } from '../../models/interfaces/request';
+import { UserProvider } from '../user/user';
 import firebase from 'firebase';
 
 /*
@@ -11,7 +13,11 @@ import firebase from 'firebase';
 @Injectable()
 export class RequestsProvider {
   firereq = firebase.database().ref('/requests');
-  constructor() {
+  firefriends = firebase.database().ref('/friends');
+
+  userdetails;
+  myfriends;
+  constructor(public userservice: UserProvider, public events: Events) {
 
   }
 
@@ -21,10 +27,95 @@ export class RequestsProvider {
         sender: req.sender
       }).then(() => {
         resolve({ success: true });
-      }).catch((err) => {
-        resolve(err);
       })
-    })
+    });
     return promise;
   }
+
+  getmyrequests() {
+    let allmyrequests;
+    var myrequests = [];
+    this.firereq.child(firebase.auth().currentUser.uid).on('value', (snapshot) => {
+      allmyrequests = snapshot.val();
+      myrequests = [];
+      for (var i in allmyrequests) {
+        myrequests.push(allmyrequests[i].sender);
+      }
+      this.userservice.getallusers().then((res) => {
+        var allusers = res;
+        this.userdetails = [];
+        for (var j in myrequests)
+          for (var key in allusers) {
+            if (myrequests[j] === allusers[key].uid) {
+              this.userdetails.push(allusers[key]);
+            }
+          }
+        this.events.publish('gotrequests');
+      })
+
+    })
+  }
+
+  acceptrequest(buddy) {
+    var promise = new Promise((resolve, reject) => {
+      this.myfriends = [];
+      this.firefriends.child(firebase.auth().currentUser.uid).push({
+        uid: buddy.uid
+      }).then(() => {
+        this.firefriends.child(buddy.uid).push({
+          uid: firebase.auth().currentUser.uid
+        }).then(() => {
+          this.deleterequest(buddy).then(() => {
+            resolve(true);
+          })
+
+        })
+      })
+    });
+    return promise;
+  }
+
+  deleterequest(buddy) {
+    var promise = new Promise((resolve, reject) => {
+      this.firereq.child(firebase.auth().currentUser.uid).orderByChild('sender').equalTo(buddy.uid).once('value', (snapshot) => {
+        let somekey;
+        for (var key in snapshot.val())
+          somekey = key;
+        this.firereq.child(firebase.auth().currentUser.uid).child(somekey).remove().then(() => {
+          resolve(true);
+        })
+      })
+        .then(() => {
+
+        }).catch((err) => {
+        reject(err);
+      })
+    });
+    return promise;
+  }
+
+  getmyfriends() {
+    let friendsuid = [];
+    this.firefriends.child(firebase.auth().currentUser.uid).on('value', (snapshot) => {
+      let allfriends = snapshot.val();
+      this.myfriends = [];
+      for (var i in allfriends)
+        friendsuid.push(allfriends[i].uid);
+
+      this.userservice.getallusers().then((users) => {
+        this.myfriends = [];
+        for (var j in friendsuid)
+          for (var key in users) {
+            if (friendsuid[j] === users[key].uid) {
+              this.myfriends.push(users[key]);
+            }
+          }
+        this.events.publish('friends');
+      }).catch((err) => {
+        alert(err);
+      })
+
+    })
+  }
+
 }
